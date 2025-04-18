629b9a0ebefc238bf0caf162b4f5e8927d06016b
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-04-19 00:47:18 +0100

    Create part1.py

---
+++ 2024/06/part1.py
@@ -0,0 +1,28 @@
+filename = input() + ".txt"
+
+map = open(filename).read().split('\n')
+m = len(map[0])
+n = len(map)
+
+i,j = 0, 0
+while map[j][i] != '^':
+    i += 1
+    if i == m:
+        i = 0
+        j += 1
+
+k = 0
+dirs = [(0,-1), (1,0), (0,1), (-1,0)]
+nei_i, nei_j = i + dirs[k][0], j + dirs[k][1]
+visited = [(i,j)]
+
+while 0 <= nei_i < m and 0 <= nei_j < n:
+    if map[nei_j][nei_i] == '#':
+        k = (k + 1) % 4
+        nei_i, nei_j = i + dirs[k][0], j + dirs[k][1]
+    else:
+        i, j, nei_i, nei_j = nei_i, nei_j, i + dirs[k][0], j + dirs[k][1]
+        if (i,j) not in visited:
+            visited.append((i,j))
+
+print(len(visited))
