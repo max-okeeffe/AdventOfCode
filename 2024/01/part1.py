25b29a34383b20067394d448d45787f9151ab6fe
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-04-18 16:21:12 +0100

    Update part1.py

--- 2024/01/part1.py
+++ 2024/01/part1.py
@@ -1,1 +1,6 @@
+filename = input() + '.txt'
 
+pairs, N = [ row.split() for row in open(filename).read().split('\n')], len(open(filename).read().split('\n'))
+list1, list2 = sorted([int(item[0]) for item in pairs]), sorted([int(item[1]) for item in pairs])
+
+print(sum(abs(list1[i] - list2[i]) for i in range(N)))
