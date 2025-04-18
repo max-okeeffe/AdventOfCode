2966bab3f5ccfc0f934bffae099f25f280c4e6f7
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-04-18 16:45:15 +0100

    Create part2.py

---
+++ 2024/02/part2.py
@@ -0,0 +1,19 @@
+#filename = input() + '.txt'
+rows = [ [int(item) for item in row.split()] for row in open(filename).read().split('\n')]
+
+def increasing(row):
+    return all(row[i] < row[i+1] for i in range(len(row)-1))
+
+def decreasing(row):
+    return all(row[i] > row[i+1] for i in range(len(row)-1))
+
+def smooth(row):
+    return all(abs(row[i] - row[i+1]) <= 3 for i in range(len(row)-1))
+
+def safe(row):
+    return (increasing(row) or decreasing(row)) and smooth(row)
+
+def makesafe(row):
+    return any(safe(row[:i] + row[i+1:]) for i in range(len(row)))
+
+print(sum(makesafe(row) for row in rows))
