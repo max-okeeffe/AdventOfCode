728ce1e28b6cc022addf51f1f2c7ca5fcf27f885
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-04-20 22:06:17 +0100

    Create part2.py

---
+++ 2024/13/part2.py
@@ -0,0 +1,13 @@
+import re
+filename = input() + ".txt"
+machines = [[int(char) for line in machine.split('\n') for char in re.findall(r'\d+', line)] for machine in open(filename).read().split('\n\n')]
+
+def cost(machine):
+    a,c,b,d,x,y = machine
+    x, y = x + 10000000000000, y + 10000000000000
+    det = a * d - b * c
+    if (d * x - b * y) % det == 0 and (a * y - c * x) % det == 0:
+        return 3 * (d * x - b * y) // det + (a * y - c * x) // det
+    return 0
+
+print(sum(cost(machine) for machine in machines))
