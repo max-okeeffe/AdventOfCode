7871fe43016428bbe486c662a2dd03173cc834eb
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-07-15 03:37:09 +0100

    Create part2.py

---
+++ 2023/11/part2.py
@@ -0,0 +1,18 @@
+filename = input() + ".txt"
+plan = open(filename).read().split('\n')
+
+m, n = len(plan), len(plan[0])
+cols = [j for j in range(n) if all(plan[i][j] == '.' for i in range(m))]
+rows = [i for i in range(m) if all(plan[i][j] == '.' for j in range(n))]
+
+galaxies = [(i,j) for i in range(m) for j in range(n) if plan[i][j] == '#']
+N = len(galaxies)
+
+count = 0
+for i in range(N-1):
+    for j in range(i+1, N):
+        a, b = galaxies[i]
+        x, y = galaxies[j]
+        count += abs(a-x) + abs(b-y) + sum(999999 for row in rows if a <= row <= x or x <= row <= a) + sum(999999 for col in cols if b <= col <= y or y <= col <= b)
+
+print(count)
