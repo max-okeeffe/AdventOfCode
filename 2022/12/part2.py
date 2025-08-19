b5009e6b4d4db2164e1f640a631c3914ba07baa7
max-okeeffe <150198739+max-okeeffe@users.noreply.github.com>
2025-08-19 14:23:35 +0100

    Create part2.py

---
+++ 2022/12/part2.py
@@ -0,0 +1,51 @@
+from heapq import heapify, heappop, heappush
+
+filename = input() + ".txt"
+plan = open(filename).read().split('\n')
+
+m,n = len(plan), len(plan[0])
+
+def find(val):
+    for i in range(m):
+        for j in range(n):
+            if plan[i][j] == val:
+                return i, j
+
+end = find('E')
+
+def val(x):
+    if x == 'S':
+        return ord('a')
+    if x == 'E':
+        return ord('z')
+    return ord(x)
+
+def neis(node):
+    i, j = node
+    return [(x,y) for x,y in ((i+1,j), (i-1,j), (i,j+1), (i,j-1)) if 0 <= x < m and 0 <= y < n and val(plan[x][y]) - val(plan[i][j]) <= 1]
+
+starts = [(i,j) for i in range(m) for j in range(n) if plan[i][j] in 'aS']
+
+def dijkstra(start):
+
+    dis = {(i,j) : float('inf') for i in range(m) for j in range(n)}
+    dis[start] = 0
+
+    Q = [(0,start)]
+    heapify(Q)
+    visited = set()
+    while Q:
+        d, minn = heappop(Q)
+
+        if minn in visited:
+            continue
+        visited.add(minn)
+
+        for nei in neis(minn):
+            temp = d + 1
+            if temp < dis[nei]:
+                dis[nei] = temp
+                heappush(Q, (temp, nei))
+    return dis[end]
+    
+print(min(dijkstra(node) for node in starts))
